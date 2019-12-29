3f2242a8536ec4fd4456e2869a02fb6850ac8df2
LewJun <lewjun072@sina.com>
2019-12-29 22:16:47 +0800

    对外提供success和failure方法

--- src/main/java/com/example/lewjun/utils/ServletUtils.java
+++ src/main/java/com/example/lewjun/utils/ServletUtils.java
@@ -1,6 +1,7 @@
 package com.example.lewjun.utils;
 
 import com.example.lewjun.base.ApiResult;
+import com.example.lewjun.base.EnumApiResultCode;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,31 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/** @author huiye */
 public class ServletUtils {
   private static final Logger LOGGER = LoggerFactory.getLogger(ServletUtils.class);
 
   private static final String VIEW_PATH = "/WEB-INF/jsp/";
   private static final String VIEW_SUFFIX = ".jsp";
 
-  public static void toJson(HttpServletResponse resp) throws IOException {
-    toJson(resp, new ApiResult<>());
+  public static void success(HttpServletResponse resp) throws IOException {
+    toJson(resp, ApiResult.success());
   }
 
-  public static void toJson(HttpServletResponse resp, ApiResult<Object> apiResult)
+  public static void success(HttpServletResponse resp, Object data) throws IOException {
+    toJson(resp, ApiResult.success(data));
+  }
+
+  public static void failure(HttpServletResponse resp, EnumApiResultCode apiResultCode)
       throws IOException {
-    String ret =
-            GsonUtils.getGson(false, DateFormatUtils.ISO_DATE_FORMAT).toJson(apiResult);
+    toJson(resp, ApiResult.failure(apiResultCode));
+  }
+
+  private static void toJson(HttpServletResponse resp, ApiResult apiResult) throws IOException {
+    String ret = GsonUtils.getGson(false, DateFormatUtils.ISO_DATE_FORMAT).toJson(apiResult);
     LOGGER.info("{}", ret);
     resp.setContentType("application/json;charset=utf-8");
     PrintWriter writer = resp.getWriter();
-    int code = apiResult.getCode();
-
-    // 在200到600外使用200作为状态码
-    if (code >= 200 && code < 600) {
-      resp.setStatus(code);
-    } else {
-      resp.setStatus(200);
-    }
     writer.println(ret);
     writer.flush();
   }
