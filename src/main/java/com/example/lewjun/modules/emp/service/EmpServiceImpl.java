f98dcae9ed379d3c63326de3e5543822c35a5928
LewJun <lewjun072@sina.com>
2020-01-01 11:42:13 +0800

    继承自BaseServiceImpl，实现EmpService，覆盖setRepository方法

--- src/main/java/com/example/lewjun/modules/emp/service/EmpServiceImpl.java
+++ src/main/java/com/example/lewjun/modules/emp/service/EmpServiceImpl.java
@@ -1,48 +1,26 @@
 package com.example.lewjun.modules.emp.service;
 
+import com.example.lewjun.base.BaseRepository;
+import com.example.lewjun.base.BaseServiceImpl;
 import com.example.lewjun.modules.emp.dao.EmpRepository;
 import com.example.lewjun.modules.emp.dao.InMemEmpRepository;
 import com.example.lewjun.modules.emp.model.Emp;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 
-public class EmpServiceImpl implements EmpService {
-  private static final Logger LOGGER = LoggerFactory.getLogger(EmpServiceImpl.class);
-  private final EmpRepository empRepository;
+/** @author huiye */
+public class EmpServiceImpl extends BaseServiceImpl<Emp, Integer> implements EmpService {
 
-  public EmpServiceImpl() {
+  private EmpRepository empRepository;
+
+  @Override
+  protected BaseRepository<Emp, Integer> setRepository() {
     empRepository = new InMemEmpRepository();
-  }
-
-  @Override
-  public void add(Emp t) {
-    empRepository.add(t);
-  }
-
-  @Override
-  public void delete(Integer pk) {
-    empRepository.remove(pk);
-  }
-
-  @Override
-  public Emp findById(Integer pk) {
-    return empRepository.get(pk);
+    return empRepository;
   }
 
   @Override
   public Collection<Emp> findByName(String name) {
     return empRepository.findByName(name);
-  }
-
-  @Override
-  public Collection<Emp> getAll() {
-    return empRepository.getAll();
-  }
-
-  @Override
-  public void update(Emp t) {
-    empRepository.update(t);
   }
 }
